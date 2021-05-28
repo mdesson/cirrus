495dc1492ab9e144479d28fc41323e27587551c9
Michael Desson <michael.desson@gmail.com>
2021-05-28 18:35:32 -0400

    Refactored to use openweathermap's onecall endpoint

--- maple/maple.go
+++ maple/maple.go
@@ -23,55 +23,111 @@ func getConfig(filename string) (MapleConfig, error) {
 		return MapleConfig{}, fmt.Errorf("error reading file: %v", err)
 	}
 
-	// config := MapleConfig{}
 	json.Unmarshal(bytes, &config)
 
 	return config, nil
 }
 
-func getCurrentConditions() (CurrentConditions, error) {
-	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?lat=%v&lon=%v&appid=%v&units=%v", config.Latitude, config.Longitude, config.OpenWeatherApiKey, config.Unit)
+func getCurrentConditions() (WeatherData, error) {
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/onecall?lat=%v&lon=%v&appid=%v&units=%v",
+		config.Latitude, config.Longitude, config.OpenWeatherApiKey, config.Unit)
+
 	resp, err := http.Get(url)
 	if err != nil {
-		return CurrentConditions{}, fmt.Errorf("error in http request: %v", err)
+		return WeatherData{}, fmt.Errorf("error in http request: %v", err)
 	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return CurrentConditions{}, fmt.Errorf("error reading file: %v", err)
+		return WeatherData{}, fmt.Errorf("error reading file: %v", err)
 	}
-	weatherJson := struct {
-		Weather []struct {
-			Description string `json:"description"`
-		} `json:"weather"`
 
-		Main struct {
+	// One-off struct to hold openeweathermap's API response
+	weatherJson := struct {
+		Current struct {
+			Dt        int     `json:"dt"`
 			Temp      float64 `json:"temp"`
 			FeelsLike float64 `json:"feels_like"`
 			Pressure  int     `json:"pressure"`
 			Humidity  int     `json:"humidity"`
-		} `json:"main"`
-		Wind struct {
-			Speed float64 `json:"speed"`
-			Deg   int     `json:"deg"`
-		} `json:"wind"`
-		Dt int `json:"dt"`
+			WindSpeed float64 `json:"wind_speed"`
+			WindDeg   int     `json:"wind_deg"`
+			Weather   []struct {
+				Description string `json:"description"`
+			} `json:"weather"`
+		} `json:"current"`
+		Minutely []PrecipitationChance `json:"minutely"`
+		Hourly   []struct {
+			Dt        int     `json:"dt"`
+			Temp      float64 `json:"temp"`
+			FeelsLike float64 `json:"feels_like"`
+			WindSpeed float64 `json:"wind_speed"`
+			WindGust  float64 `json:"wind_gust"`
+			Weather   []struct {
+				Description string `json:"description"`
+			} `json:"weather"`
+			Pop int `json:"pop"`
+		} `json:"hourly"`
+		Daily []struct {
+			Dt        int               `json:"dt"`
+			Temp      TempForecast      `json:"temp"`
+			FeelsLike FeelsLikeForecast `json:"feels_like"`
+			Humidity  int               `json:"humidity"`
+			WindSpeed float64           `json:"wind_speed"`
+			WindGust  float64           `json:"wind_gust"`
+			Weather   []struct {
+				Description string `json:"description"`
+			} `json:"weather"`
+			Pop  int     `json:"pop"`
+			Rain float64 `json:"rain,omitempty"`
+		} `json:"daily"`
 	}{}
 
 	json.Unmarshal(body, &weatherJson)
 
-	currentConditions := CurrentConditions{
-		Temperature:   weatherJson.Main.Temp,
-		FeelsLike:     weatherJson.Main.FeelsLike,
-		Humidity:      weatherJson.Main.Humidity,
-		WindSpeed:     weatherJson.Wind.Speed,
-		WindDirection: weatherJson.Wind.Deg,
-		Pressure:      weatherJson.Main.Pressure,
-		Condition:     weatherJson.Weather[0].Description,
-		Timestamp:     weatherJson.Dt,
+	data := WeatherData{}
+
+	data.Current = CurrentConditions{
+		Timestamp:     weatherJson.Current.Dt,
+		Temp:          weatherJson.Current.Temp,
+		FeelsLike:     weatherJson.Current.FeelsLike,
+		Pressure:      weatherJson.Current.Pressure,
+		Humidity:      weatherJson.Current.Humidity,
+		WindSpeed:     weatherJson.Current.WindSpeed,
+		WindDirection: weatherJson.Current.WindDeg,
+		Description:   weatherJson.Current.Weather[0].Description,
 	}
 
-	return currentConditions, nil
+	data.Minutely = weatherJson.Minutely
+
+	data.Hourly = make([]HourlyForecast, 48)
+	for i, forecast := range weatherJson.Hourly {
+		data.Hourly[i] = HourlyForecast{
+			Timestamp:   forecast.Dt,
+			Temp:        forecast.Temp,
+			FeelsLike:   forecast.FeelsLike,
+			WindSpeed:   forecast.WindSpeed,
+			WindGust:    forecast.WindGust,
+			Description: forecast.Weather[0].Description,
+			Pop:         forecast.Pop,
+		}
+	}
+
+	data.Daily = make([]DailyForecast, 7)
+	for i, forecast := range weatherJson.Daily[1:] {
+		data.Daily[i] = DailyForecast{
+			Timestamp:   forecast.Dt,
+			Humidity:    forecast.Humidity,
+			WindSpeed:   forecast.WindSpeed,
+			WindGust:    forecast.WindGust,
+			Description: forecast.Weather[0].Description,
+			Temp:        forecast.Temp,
+			FeelsLike:   forecast.FeelsLike,
+		}
+	}
+
+	return data, nil
 }
 
 func main() {
@@ -82,9 +138,9 @@ func main() {
 	}
 
 	fmt.Println(config)
-	current, err := getCurrentConditions()
+	data, err := getCurrentConditions()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%+v\n", current)
+	fmt.Printf("%+v\n", data)
 }
